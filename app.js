f3399920a6e7565741dab78d2c1afb6884130d16
roberto641 <roberto@unisobrasil.com.br>
2026-09-23 12:28:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from
 import { getFirestore, doc, getDoc, collection, query, where, orderBy, limit, getDocs } from 'https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js';
 import { getFunctions, httpsCallable } from 'https://www.gstatic.com/firebasejs/11.10.0/firebase-functions.js';
 import { firebaseConfig } from './config.js';
-import import './scoring.js';
+import './scoring.js';
 const { CRITERIA, calculate } = globalThis.UNISO_SCORING;
 const $ = id => document.getElementById(id);
 if (Object.values(firebaseConfig).some(v => v === 'PREENCHER')) $('setup').hidden = false;
